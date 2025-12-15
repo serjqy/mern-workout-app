35a8cab6f1250f0dad87f4537430ce2e04b21e6e
serjay <serhiinote@gmail.com>
2025-12-15 20:03:17 +0000

    Handle Errors

--- backend/controllers/WorkoutController.js
+++ backend/controllers/WorkoutController.js
@@ -27,6 +27,24 @@ export const getWorkout = async (req, res) => {
 // Create New Workout
 export const createWorkout = async (req, res) => {
   const { title, reps, load } = req.body;
+
+  let emptyFields = [];
+
+  if (!title) {
+    emptyFields.push("title");
+  }
+  if (!load) {
+    emptyFields.push("load");
+  }
+  if (!reps) {
+    emptyFields.push("reps");
+  }
+  if (emptyFields.length > 0) {
+    return res
+      .status(400)
+      .json({ error: "Please fill in all the fields", emptyFields });
+  }
+
   // Add document to DB
   try {
     const workout = await Workout.create({
