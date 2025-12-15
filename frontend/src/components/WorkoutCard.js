9da1d3d35ac81ab0e31a47460f6e52580231eb03
serjay <serhiinote@gmail.com>
2025-12-15 19:54:49 +0000

    Delete Workout

--- frontend/src/components/WorkoutCard.js
+++ frontend/src/components/WorkoutCard.js
@@ -1,6 +1,21 @@
 import React from "react";
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
 
 const WorkoutCard = ({ workout }) => {
+  const { dispatch } = useWorkoutsContext();
+
+  const handleClick = async () => {
+    const response = await fetch(`/api/workouts/${workout._id}`, {
+      method: "DELETE",
+    });
+
+    const json = await response.json();
+
+    if (response.ok) {
+      dispatch({ type: "DELETE_WORKOUT", payload: json });
+    }
+  };
+
   return (
     <div className="workout-details">
       <h4>{workout.title}</h4>
@@ -13,6 +28,7 @@ const WorkoutCard = ({ workout }) => {
         {workout.reps}
       </p>
       <p>{workout.createdAt}</p>
+      <span onClick={handleClick}>Delete</span>
     </div>
   );
 };
