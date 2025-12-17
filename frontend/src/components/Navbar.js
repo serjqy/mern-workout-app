054972c767a3b1634b1290f325881b9ea84254d9
serjay <serhiinote@gmail.com>
2025-12-17 16:52:04 +0000

    Initial Auth Status

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,9 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useLogout } from "../hooks/useLogout";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 const Navbar = () => {
   const { logout } = useLogout();
+
+  const { user } = useAuthContext();
 
   const handleClick = () => {
     logout();
@@ -16,13 +19,18 @@ const Navbar = () => {
           <h1>Workout Buddies</h1>
         </Link>
         <nav>
-          <div>
-            <button onClick={handleClick}>Log Out</button>
-          </div>
-          <div>
-            <Link to="/login">Login</Link>
-            <Link to="/register">Register</Link>
-          </div>
+          {user && (
+            <div>
+              <span>{user.email}</span>
+              <button onClick={handleClick}>Log Out</button>
+            </div>
+          )}
+          {!user && (
+            <div>
+              <Link to="/login">Login</Link>
+              <Link to="/register">Register</Link>
+            </div>
+          )}
         </nav>
       </div>
     </header>
