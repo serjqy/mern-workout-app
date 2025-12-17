b2c3d649754f9466c253fd7536eb6334c7fbe0e5
serjay <serhiinote@gmail.com>
2025-12-17 15:44:04 +0000

    Login Method

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-import bcrypt from "bcrypt";
+import bcrypt, { hash } from "bcrypt";
 import validator from "validator";
 
 const Schema = mongoose.Schema;
@@ -41,6 +41,28 @@ userSchema.statics.register = async function (email, password) {
   const hash = await bcrypt.hash(password, salt);
 
   const user = await this.create({ email, password: hash });
+
+  return user;
+};
+
+// Static Login Method
+userSchema.statics.login = async function (email, password) {
+  // Validation
+  if (!email || !password) {
+    throw Error("All fields must be filled");
+  }
+
+  const user = await this.findOne({ email });
+
+  if (!user) {
+    throw Error("Incorrect email");
+  }
+
+  const match = await bcrypt.compare(password, user.password);
+
+  if (!match) {
+    throw Error("Incorrect password");
+  }
 
   return user;
 };
